68ddd6cacbc56031a647df6fc41c869b14d19265
Luis Dias <luis.edudias@gmail.com>
2013-05-23 20:23:24 +0100

    update

--- app/config/database.php
+++ app/config/database.php
@@ -79,7 +79,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'salteste',
+		'database' => 'sal',
                 'encoding' => 'utf8'
 	);
 
