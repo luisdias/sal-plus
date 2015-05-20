c91d09d0ad1ff1000463e35e13a9043e978dcea7
Luis Dias <luis.edudias@gmail.com>
2015-05-20 14:20:27 +0100

    bugfix: plugin = null in logout redirect

--- app/app_controller.php
+++ app/app_controller.php
@@ -26,7 +26,7 @@
             $this->Auth->fields = array( 'username' => 'login','password' => 'senha');
             $this->Auth->loginAction=array('controller'=>'usuarios','action'=>'login');
             $this->Auth->loginRedirect=array('controller'=>'licitacoes','action'=>'index');
-            $this->Auth->logoutRedirect=array('controller'=>'usuarios','action'=>'login');
+            $this->Auth->logoutRedirect=array('controller'=>'usuarios','action'=>'login','plugin'=>null);
             $this->Auth->loginError=__('Login ou senha inválido',true);     
             $this->Auth->authError=__('Sem permissão de acesso.',true); 
             $this->Auth->authorize = 'controller';            
